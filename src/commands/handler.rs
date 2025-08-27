3a5be220fd46d0d61d59d0368ad9c91875e00976
Tonkku <contact@tonkku.me>
2025-08-27 14:14:36 +0300

    Parse label

--- src/commands/handler.rs
+++ src/commands/handler.rs
@@ -299,6 +299,9 @@ impl CommandHandler {
         Component::ActionRow(action_row) => {
           Self::parse_component_values(action_row.components, input);
         },
+        Component::Label(label) => {
+          Self::parse_component_values(vec![*label.component], input);
+        },
         Component::TextInput(text_input) => {
           let value = OptionValue::String(text_input.value.unwrap_or_default());
           input.args.insert(text_input.custom_id, value);
